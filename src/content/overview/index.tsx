f2565f4bde6ab853c7229939989819bc55a8b432
giyeol <bae@giyeol.com>
2022-06-13 00:31:01 +0900

    fix: change helmet useage

--- src/content/overview/index.tsx
+++ src/content/overview/index.tsx
@@ -1,12 +1,12 @@
 import { Box, Container, Card } from '@mui/material';
-import { Helmet } from 'react-helmet-async';
+import { Helmet, HelmetProvider } from 'react-helmet-async';
 
 import { styled } from '@mui/material/styles';
 import Logo from 'components/LoginSign';
 import Hero from './Hero'
 
 const OverviewWrapper = styled(Box) (
-  () => `
+    () => `
     overflow: auto;
     flex: 1;
     overflow-x: hidden;
@@ -14,21 +14,23 @@ const OverviewWrapper = styled(Box) (
   `
 )
 const Overview = (props) => {
-  return (
-    <OverviewWrapper>
-      <Helmet>
-        <title>지윤짱</title>
-      </Helmet>
-      <Container maxWidth="lg">
-        <Box display="flex" justifyContent="center" py={5} alignItems="center">
-          <Logo />
-        </Box>
-        <Card sx={{ p: 10, mb: 10, borderRadius: 12 }}>
-          <Hero />
-        </Card>
-      </Container>
-    </OverviewWrapper>
-  )
+    return (
+        <HelmetProvider>
+            <OverviewWrapper>
+                <Helmet>
+                    <title>지윤짱</title>
+                </Helmet>
+                <Container maxWidth="lg">
+                    <Box display="flex" justifyContent="center" py={5} alignItems="center">
+                        <Logo />
+                    </Box>
+                    <Card sx={{ p: 10, mb: 10, borderRadius: 12 }}>
+                        <Hero />
+                    </Card>
+                </Container>
+            </OverviewWrapper>
+        </HelmetProvider>
+    )
 }
 
 export default Overview;
